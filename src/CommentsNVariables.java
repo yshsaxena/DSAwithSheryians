62cc5b7ef89339ffcf6d7c6101780a6b6d90193d
Yash Saxena <45199433+yshsaxena@users.noreply.github.com>
2025-02-10 00:40:28 +0530

    Day One - Installation, Hello world, comments and variables, coding conventions

---
+++ src/CommentsNVariables.java
@@ -0,0 +1,45 @@
+public class CommentsNVariables{
+    public static void main(String[] args) {
+
+        // This is Comments in a single line
+        System.out.println("Comments in single line");
+
+        /* This is a comments in Multiline
+            Line One
+            Line Two
+            Line Three
+         */
+        System.out.println("Comments in Multiline");
+
+
+        // Variable Declaration
+
+        int varA;
+
+        // Variable Initialization
+        varA = 10;
+
+        // together in single line
+
+        int varB = 10;
+
+        // plus + Operator
+
+        System.out.println(varB + varA);
+
+        // Concatination
+
+        System.out.println("Concatination of a & b" + varA + varB);
+
+        /*
+        if a string gets add with Integer it did mathematically add its just
+        to each other.
+         */
+
+        System.out.println("StringOne" + varA );
+
+        // mathematical operations
+
+        System.out.println(varB + varA); // Adding two numbers
+    }
+}
