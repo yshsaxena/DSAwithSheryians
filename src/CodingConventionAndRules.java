ca878ae27a60128a441ebaee3b3ea9b16e7216cf
Yash Saxena <yshsaxena23@gmail.com>
2025-02-10 14:40:05 +0530

    Create CodingConventionAndRules.java

---
+++ src/CodingConventionAndRules.java
@@ -0,0 +1,16 @@
+public class CodingConventionAndRules {
+    public static void main(String[] args) {
+
+        // Variable Naming rules
+        // Variable name must start with lower case
+        // example of variable names below
+        // types of casing
+        /*
+        Camel
+        Pascal
+        Snake
+        Kabab
+         */
+
+    }
+}
