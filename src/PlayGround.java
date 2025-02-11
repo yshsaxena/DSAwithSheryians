cd723171b6c4c4139ac3412b4995f24c4c6fb30b
Yash Saxena <45199433+yshsaxena@users.noreply.github.com>
2025-02-11 12:55:06 +0530

    playground changes

--- src/PlayGround.java
+++ src/PlayGround.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class PlayGround {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your age");
+//        System.out.println("Enter your age");
+//
+//        int age = sc.nextInt();
+//
+//        System.out.println(age);
+//        System.out.println("Enter you name");
+//        String name = sc.next();
+//        String remaining = sc.next();
+//        System.out.println(name);
+//        System.out.println(remaining);
 
-        int age = sc.nextInt();
 
-        System.out.println(age);
-        System.out.println("Enter you name");
-        String name = sc.next();
-        String remaining = sc.next();
-        System.out.println(name);
-        System.out.println(remaining);
+        // Main Question
+        char ch = sc.next().charAt(0);
+        String a = sc.nextLine();
+
+        System.out.println(ch);
+        System.out.println(a);
     }
 }
