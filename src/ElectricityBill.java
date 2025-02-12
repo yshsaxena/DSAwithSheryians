a9cd7f5105f74892d7a6b1723be3218957b5fcfa
Yash Saxena <45199433+yshsaxena@users.noreply.github.com>
2025-02-13 00:59:53 +0530

    bug fix
    Conditional Statements

--- src/ElectricityBill.java
+++ src/ElectricityBill.java
@@ -6,26 +6,20 @@ public class ElectricityBill {
 
         int unit = sc.nextInt();
 
-        double rate = 0.0;
-        while (unit > 0){
-            if(unit <= 100){
-                rate += unit * 4.2;
-            }
-            if ( unit > 100 && unit <= 200){
-                unit -=  100;
-                rate += unit * 6;
-            }
-            if( unit > 200 && unit <= 400){
-                unit -= 200;
-                rate += unit * 8;
-            }
-            if( unit > 400){
-                unit -= 400;
-                rate += unit * 13;
-            }
-
-
+        double rate = 0.0, u100 = 420, u200 = 1020, u400 = 2620;
+        if (unit > 0 && unit <= 100) {
+            rate = unit * 4.2;
         }
+        if (unit > 100 && unit <= 200) {
+            rate = u100 + (unit -100) * 6;
+        }
+        if (unit > 200 && unit <= 400) {
+            rate = u200 + (unit - 200) * 8;
+        }
+        if (unit > 400) {
+            rate = u100 + (unit - 400) * 13;
+        }
+
         System.out.println(rate);
     }
 }
